73ec78899688f45f72a1b02683b4448ee5ac140a
isaaccleopas <isaaccleopas@gmail.com>
2023-04-28 20:39:17 +0000

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-'''Contains the index view for the API.'''
+"""Contains the index view for the API."""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
+
+app_views.route('/status', methods=['GET'], strict_slashes=False)
+def api_status():
+    """ Returns JSON """
     return jsonify({"status": "OK"})
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    storage.close()
-
-if __name__ == '__main__':
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port)
-
