b59b9b9d8e98f836d8aa326108dd07f627ee188d
isaaccleopas <isaaccleopas@gmail.com>
2023-04-29 11:50:18 +0000

    corrections

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,17 @@ app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     """ Returns JSON """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """returns the number of each objects by type"""
+    stats_dict = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats_dict)
