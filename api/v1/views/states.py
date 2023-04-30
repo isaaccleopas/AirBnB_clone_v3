9a2b4944d00fd663f38cf1407f8a878cc2caba6a
isaaccleopas <isaaccleopas@gmail.com>
2023-04-30 14:30:50 +0000

    states task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-View for State objects that handles default RESTful API actions.
+View for State objects that handles
+default RESTful API actions.
 """
 from flask import jsonify, abort, request
 from models import storage
@@ -8,7 +9,8 @@ from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', methods=['GET'],
+                 strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects."""
     states = storage.all(State).values()
@@ -16,7 +18,8 @@ def get_states():
     return jsonify(states_list)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object."""
     state = storage.get(State, state_id)
@@ -25,7 +28,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object."""
     state = storage.get(State, state_id)
@@ -36,7 +40,8 @@ def delete_state(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
+@app_views.route('/states', methods=['POST'],
+                 strict_slashes=False)
 def create_state():
     """Creates a State."""
     data = request.get_json()
@@ -49,7 +54,8 @@ def create_state():
     return jsonify(state.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<state_id>',
+                 methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
     """Updates a State object."""
     state = storage.get(State, state_id)
