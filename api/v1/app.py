1affe2f730b42c648410e770c5dff08ba737de43
isaaccleopas <isaaccleopas@gmail.com>
2023-04-30 14:09:18 +0000

    cors task

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,6 +10,8 @@ from api.v1.views import app_views
 
 app = Flask(__name__)
 """The Flask web application instance."""
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
@@ -36,4 +38,10 @@ def error_400(error):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True )
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(
+        host=app_host,
+        port=app_port,
+        threaded=True
+    )
