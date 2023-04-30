83fc528d0c115faa05507839230c9e547354d83b
isaaccleopas <isaaccleopas@gmail.com>
2023-04-30 11:49:55 +0000

    task 7

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,13 +5,15 @@ from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route('/status', methods=['GET'],
+                 strict_slashes=False)
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route('/stats',
+                 methods=['GET'], strict_slashes=False)
 def stat():
     """returns the number of each objects by type"""
     return jsonify(
